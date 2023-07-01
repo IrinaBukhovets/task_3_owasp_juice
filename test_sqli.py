f7497946cb35d750979fe85dccef60c49ec9c14b
Irina <buhovets.i@yandex.ru>
2023-07-01 15:36:32 +0300

    test_sqli

--- test_sqli.py
+++ test_sqli.py
@@ -1,14 +1,21 @@
-from task_3_owasp_juice.Juiceshop import LandingPage
-from task_3_owasp_juice.Juiceshop import LoginPage
+from Juiceshop import LandingPage
+from Juiceshop import LoginPage
+from Juiceshop import Label
 import time
 
 def test_sqli(browser):
+    close_label = Label()
+    close_label.close_label()
+    time.sleep(2)
     log_in_account = LandingPage()
     log_in_account.account()
     time.sleep(2)
     input_login_and_password = LoginPage()
     input_login_and_password.input_login_and_password("' or true--","12345")
+    #"' or true--"
+    time.sleep(2)
     error_message = LoginPage()
-    error_message.error_message()
-    assert error_message == "Invalid email or password."
+    error = error_message.text_error_message_invalid_log_pass()
+    assert error == "Invalid email or password."
+    
 
