8f4dd26e0b0e6603f7798b6c05594a09bf48bed1
Irina <buhovets.i@yandex.ru>
2023-07-02 14:13:12 +0300

    test_log_pass_strength

---
+++ test_log_pass_strength_copy.py
@@ -0,0 +1,21 @@
+from Juiceshop import LandingPage
+from Juiceshop import LoginPage
+from Juiceshop import Label
+import pytest
+
+
+@pytest.mark.parametrize("email", ['stan@juice-sh.op', 'admin@juice-sh.op'])
+@pytest.mark.parametrize("password", ['Aa123456', ')4ever', 'admin123', '1qaz@WSX', 'P@ssw0rd','123456QQAqqa_','1qaz!QAZ','wowecarts@123','film@123','Spiritwear_2004'])  
+@pytest.mark.parametrize ("result", ['Invalid email or password.'])                 
+
+def test_log_pass_strength(browser, email, password, result):
+    close_label = Label()
+    close_label.close_label()
+    log_in_account = LandingPage()
+    log_in_account.account()
+    input_login_and_password = LoginPage()
+    input_login_and_password.input_login_and_password(login=email,password=password)
+    error_message = LoginPage()
+    error = error_message.text_error_message_invalid_log_pass()
+    assert error == result
+    
